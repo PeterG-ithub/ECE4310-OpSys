69af187bd0cfc2fc161bf6b7ccbf94a553c7d68e
Peter Gabradilla <98351061+PeterGabradilla@users.noreply.github.com>
2022-12-01 10:59:12 -0800

    Update user.c

--- Seminar_4/user.c
+++ Seminar_4/user.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+int main()
+{
+    printf("Hi world\n");
+    int fp = open("/dev/test_mod02", O_RDWR);
+    if(fp < 0)
+    {
+        printf("Open Error\n");
+        return -1;
+    }
+
+    char buffer[64];
+    read(fp, buffer, 10);
+    printf("%s\n\n", buffer);
+    return 0;
+}
